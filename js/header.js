fbd3b643e9ce03aac54891805e82024db5d2acf5
nanathar <lewalskimateusz94@gmail.com>
2022-10-19 13:08:53 +0200

    Add new project

--- js/header.js
+++ js/header.js
@@ -1,13 +1,17 @@
 const body = document.body;
 let lastScroll = 0;
+const div = document.querySelector('div.menu-hamburger');
+
 
 window.addEventListener("scroll", () => {
   const currentScroll = window.pageYOffset;
 
-  if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
-    body.classList.add("scroll-down");
-  } else if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
-    body.classList.remove("scroll-down");
+  if (div.className === 'menu-hamburger') {
+    if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+      body.classList.add("scroll-down");
+    } else if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
+      body.classList.remove("scroll-down");
+    }
+    lastScroll = currentScroll;
   }
-  lastScroll = currentScroll;
 });
